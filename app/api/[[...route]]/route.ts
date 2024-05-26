07b71da2e03277e9930e08110b2ec18300ed5287
lkereceni <lovro.kereceni@gmail.com>
2024-05-26 23:21:10 +0200

    First commit

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,28 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+
+import accounts from "./accounts";
+import { HTTPException } from "hono/http-exception";
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api");
+
+app.onError((error, c) => {
+  if (error instanceof HTTPException) {
+    return error.getResponse();
+  }
+
+  return c.json({ error: "Internal error" }, 500);
+});
+
+const routes = app.route("/accounts", accounts);
+
+export const GET = handle(app);
+export const POST = handle(app);
+export const PATCH = handle(app);
+
+export type AppType = typeof routes;
